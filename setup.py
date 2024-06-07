2dfbd159e709df101defd9b3321f0a48cd3a654b
KenjiKamimoto-wustl122 <kamimoto@wustl.edu>
2024-06-07 10:45:06 -0500

    add tables to requirement

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open('LICENSE') as f:
 
 setup(
     name='celloracle_streamlit',
-    version="0.4.4",
+    version="0.5.0",
     description='celloracle_utility_functions',
     long_description=readme,
     python_requires='>=3.6',
